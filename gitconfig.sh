0fcab8f3fb009c011239610c6802c5d1b9f3a82b
Aaron Anderson <a622aron.fjip@gmail.com>
2019-01-18 18:43:16 -0600

    git: Add git branch alias

--- gitconfig.sh
+++ gitconfig.sh
@@ -6,9 +6,10 @@ GIT=git
 
 ${GIT} config --global alias.st     "status"
 ${GIT} config --global alias.ck     "checkout"
+${GIT} config --global alias.b      "branch"
 ${GIT} config --global alias.absorb "commit --amend --no-edit"
 ${GIT} config --global alias.sub    "submodule"
 ${GIT} config --global alias.sst    "submodule status"
 ${GIT} config --global alias.sup    "submodule update"
 ${GIT} config --global alias.last   "show --stat HEAD"
-${GIT} config --global alias.t  "log --graph --decorate --oneline --branches"
+${GIT} config --global alias.t      "log --graph --decorate --oneline --branches"
